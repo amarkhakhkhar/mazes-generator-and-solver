81f9c8f05ca2d98563662c3b696494d8d26ecdba
Amar Khakhkhar <amarkhakhkhar2241566@gmail.com>
2023-07-21 17:22:04 +0530

    download issue solved

--- scripts/download.js
+++ scripts/download.js
@@ -1,7 +1,6 @@
-function download(name) {
-    var download = document.getElementById("download");
+function download(name, id) {
+    var download = document.getElementById(id);
     var image = document.getElementById(name).toDataURL("image/png")
         .replace("image/png", "image/octet-stream");
     download.setAttribute("href", image);
-    //download.setAttribute("download","archive.png");
 }
